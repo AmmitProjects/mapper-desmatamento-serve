02d4d7b494ea41741016e2737644b03a0b52ae25
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-12-06 01:23:10 -0300

    codificação das regras de negócio no projeto da api

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, jsonify
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def calcular_desmatamento():
+    try:
+        # Definindo manualmente os caminhos das imagens
+        foto_ano_anterior = r'C:\Users\eduar\PycharmProjects\MapperDesmatamentoServe\imgs\2022.jpg'
+        foto_ano_atual = r'C:\Users\eduar\PycharmProjects\MapperDesmatamentoServe\imgs\2021.jpg'
+
+        # Lendo as imagens
+        img_ano_anterior = cv2.imread(foto_ano_anterior)
+        img_ano_atual = cv2.imread(foto_ano_atual)
+
+        # Convertendo as imagens para escala de cinza
+        gray_ano_anterior = cv2.cvtColor(img_ano_anterior, cv2.COLOR_BGR2GRAY)
+        gray_ano_atual = cv2.cvtColor(img_ano_atual, cv2.COLOR_BGR2GRAY)
+
+        # Calculando a diferença entre as imagens
+        diff = cv2.absdiff(gray_ano_anterior, gray_ano_atual)
+
+        # Aplicando um limiar para destacar as diferenças
+        _, threshold_diff = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+
+        # Calculando a porcentagem de desmatamento
+        porcentagem_desmatamento = (np.count_nonzero(threshold_diff) / threshold_diff.size) * 100
+
+        return porcentagem_desmatamento
+
+    except Exception as e:
+        return str(e)
+
+@app.route('/comparar_desmatamento/', methods=['GET'])
+def comparar_desmatamento_endpoint():
+    porcentagem_desmatamento = calcular_desmatamento()
+
+    if isinstance(porcentagem_desmatamento, str):
+        return jsonify({'erro': porcentagem_desmatamento}), 500
+    else:
+        return jsonify({'porcentagem_desmatamento': porcentagem_desmatamento}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
